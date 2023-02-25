41ff37ee1d363b5793895eaedceb95252bc86151
Jackaed <ed.andy.jackson@gmail.com>
2023-02-25 22:09:32 +0000

    Revert PieceData comment

--- Assets/Scripts/Core/Pieces/PieceData.cs
+++ Assets/Scripts/Core/Pieces/PieceData.cs
@@ -2,6 +2,11 @@
 
 namespace Antichess.Core.Pieces
 {
+    /// <summary>
+    /// An abstract data type which represents and stores all of the information pertaining to how
+    /// a specific piece should be regarded in a given situation. Distinct from Piece, as this only
+    /// defines how types of pieces act, rather than the data associated with each instance of a piece.
+    /// </summary>
     public interface IPieceData
     {
         public GameObject BlackModel { get; }
